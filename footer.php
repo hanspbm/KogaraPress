82ad7628965ec486a58361080ec644c79f15a601
hanspbm <hanspbm@gmail.com>
2018-02-11 15:58:04 -0500

    Minor styling fixes. Moved Follow Us section to footer template.

--- footer.php
+++ footer.php
@@ -9,6 +9,37 @@
  */
 ?>
 
+<section id="follow-us">
+	<h2>Follow Us</h2>
+
+	<div class="grid-x grid-padding-x align-center">
+		<div class="cell shrink">
+			<a href="https://www.facebook.com/kogaracon/" target="_blank">
+				<span class="fa-stack fa-lg">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+				</span>
+			</a>
+		</div>
+		<div class="cell shrink">
+			<a href="https://twitter.com/kogaracon/" target="_blank">
+				<span class="fa-stack fa-lg">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+				</span>
+			</a>
+		</div>
+		<div class="cell shrink">
+			<a href="https://www.instagram.com/kogaracon/" target="_blank">
+				<span class="fa-stack fa-lg">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+				</span>
+			</a>
+		</div>
+	</div>
+</section>
+
 <footer class="footer">
     <div class="footer-container">
         <div class="footer-grid">
